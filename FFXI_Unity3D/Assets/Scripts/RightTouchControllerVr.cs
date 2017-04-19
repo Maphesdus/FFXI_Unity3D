24cfd091ef7b143a7f710ba060f1db82e6529dc8
gatorkea <gatorkea@yahoo.com>
2017-04-19 18:20:03 -0500

    Added Valkurm, East Sarutabaruta, and various things

---
+++ FFXI_Unity3D/Assets/Scripts/RightTouchControllerVr.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RightTouchControllerVr : MonoBehaviour {
+
+	
+	
+	// Update is called once per frame
+	void Update () {
+		transform.localPosition = OVRInput.GetLocalControllerPosition(OVRInput.Controller.RTouch);
+		transform.localRotation = OVRInput.GetLocalControllerRotation(OVRInput.Controller.RTouch);
+
+		
+	}
+}
