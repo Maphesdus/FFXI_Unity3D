7689900fecfb156370a60426ef1033811265c7c0
Maphesdus <nanobot18@gmail.com>
2017-05-01 07:56:34 -0600

    Added FFXI text font, GUI buttons/textures, & Goblin + Ifirt animations
    Added FFXI text font (needs fixed), FFXI crate texture, FFXI grass texture, FFXI GUI buttons and textures, & animations for Goblin and Ifrit. See the Main scene in the Miscellaneous folder for the changes.

---
+++ FFXI_Unity3D/Assets/Code/CustomCursor.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CustomCursor : MonoBehaviour {
+	public Texture2D cursorTexture;
+	public CursorMode cursorMode = CursorMode.Auto;
+	public Vector2 hotSpot = Vector2.zero;
+
+	void Start() {
+		Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+	}
+}
---
+++ FFXI_Unity3D/Assets/Code/NPCDialogue.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCDialogue : MonoBehaviour {
+	public GameObject canvas;
+
+
+	// START:
+	void Start () { }
+
+
+	// UPDATE:
+	void Update () { }
+
+
+	// ON TRIGGER ENTER:
+	void OnTriggerEnter(Collider col) {
+		if (col.gameObject.tag == "Player") {
+			canvas.SetActive(true);
+		}
+	}
+
+
+	// ON TRIGGER EXIT:
+	void OnTriggerExit() {
+		canvas.SetActive(false);
+	}
+}
